eb28ce51f74b1cacf81d7bcc1f6d5cd9147e8240
Холли <kkxo.mail@gmail.com>
2021-04-30 18:42:53 +0300

    Команда для просмотров титулов пользователя.

---
+++ Hinode.Izumi.Services/Commands/UserCommands/UserInfoCommands/UserTitleCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Hinode.Izumi.Data.Enums;
+using Hinode.Izumi.Data.Enums.DiscordEnums;
+using Hinode.Izumi.Data.Enums.MessageEnums;
+using Hinode.Izumi.Services.Commands.Attributes;
+using Hinode.Izumi.Services.DiscordServices.DiscordEmbedService;
+using Hinode.Izumi.Services.EmoteService;
+using Hinode.Izumi.Services.EmoteService.Impl;
+using Hinode.Izumi.Services.RpgServices.UserService;
+
+namespace Hinode.Izumi.Services.Commands.UserCommands.UserInfoCommands
+{
+    [IzumiRequireContext(DiscordContext.DirectMessage), IzumiRequireRegistry]
+    public class UserTitleCommand : ModuleBase<SocketCommandContext>
+    {
+        private readonly IDiscordEmbedService _discordEmbedService;
+        private readonly IEmoteService _emoteService;
+        private readonly IUserService _userService;
+
+        public UserTitleCommand(IDiscordEmbedService discordEmbedService, IEmoteService emoteService,
+            IUserService userService)
+        {
+            _discordEmbedService = discordEmbedService;
+            _emoteService = emoteService;
+            _userService = userService;
+        }
+
+        [Command("титулы"), Alias("titles")]
+        public async Task UserTitleTask()
+        {
+            // получаем иконки из базы
+            var emotes = await _emoteService.GetEmotes();
+            // получаем титулы пользователя
+            var userTitles = await _userService.GetUserTitle((long) Context.User.Id);
+
+            var embed = new EmbedBuilder()
+                .WithDescription(
+                    IzumiReplyMessage.UserTitleDesc.Parse() +
+                    $"\n{emotes.GetEmoteOrBlank("Blank")}");
+
+            // для каждого титула создаем embed field
+            foreach (var title in Enum.GetValues(typeof(Title))
+                .Cast<Title>())
+            {
+                // если у пользователя нет титула - пропускаем
+                if (!userTitles.ContainsKey(title)) continue;
+                // если у пользователя больше 25 титулов
+                if (embed.Fields.Count == 25)
+                {
+                    embed.WithFooter("Тут отображены только первые 25 ваших титулов.");
+                    continue;
+                }
+
+                // отображем информацию о титуле
+                embed.AddField(
+                    $"{emotes.GetEmoteOrBlank("List")} `{title.GetHashCode()}` {emotes.GetEmoteOrBlank(title.Emote())} {title.Localize()}",
+                    $"{emotes.GetEmoteOrBlank("Blank")}");
+            }
+
+            await _discordEmbedService.SendEmbed(Context.User, embed);
+            await Task.CompletedTask;
+        }
+    }
+}
