d0b5a7594cf5759f3598cb92f06deeed2a2feeeb
Холли <kkxo.mail@gmail.com>
2021-05-03 10:31:32 +0300

    HD-134. Исправление отображения длительности контракта в списке контрактов.

--- Hinode.Izumi.Services/Commands/UserCommands/ContractCommands/ContractListCommand/ContractListCommand.cs
+++ Hinode.Izumi.Services/Commands/UserCommands/ContractCommands/ContractListCommand/ContractListCommand.cs
@@ -9,12 +9,14 @@ using Hinode.Izumi.Framework.Autofac;
 using Hinode.Izumi.Services.DiscordServices.DiscordEmbedService;
 using Hinode.Izumi.Services.EmoteService;
 using Hinode.Izumi.Services.EmoteService.Impl;
+using Hinode.Izumi.Services.RpgServices.CalculationService;
 using Hinode.Izumi.Services.RpgServices.ContractService;
 using Hinode.Izumi.Services.RpgServices.ImageService;
 using Hinode.Izumi.Services.RpgServices.LocalizationService;
 using Hinode.Izumi.Services.RpgServices.LocationService;
 using Hinode.Izumi.Services.RpgServices.ReputationService;
 using Hinode.Izumi.Services.RpgServices.TrainingService;
+using Hinode.Izumi.Services.RpgServices.UserService;
 using Humanizer;
 using Image = Hinode.Izumi.Data.Enums.Image;
 
@@ -31,10 +33,13 @@ namespace Hinode.Izumi.Services.Commands.UserCommands.ContractCommands.ContractL
         private readonly IReputationService _reputationService;
         private readonly ITrainingService _trainingService;
         private readonly ILocalizationService _local;
+        private readonly IUserService _userService;
+        private readonly ICalculationService _calc;
 
         public ContractListCommand(IDiscordEmbedService discordEmbedService, IEmoteService emoteService,
             ILocationService locationService, IContractService contractService, IImageService imageService,
-            IReputationService reputationService, ITrainingService trainingService, ILocalizationService local)
+            IReputationService reputationService, ITrainingService trainingService, ILocalizationService local,
+            IUserService userService, ICalculationService calc)
         {
             _discordEmbedService = discordEmbedService;
             _emoteService = emoteService;
@@ -44,16 +49,18 @@ namespace Hinode.Izumi.Services.Commands.UserCommands.ContractCommands.ContractL
             _reputationService = reputationService;
             _trainingService = trainingService;
             _local = local;
+            _userService = userService;
+            _calc = calc;
         }
 
         public async Task Execute(SocketCommandContext context)
         {
             // получаем иконки из базы
             var emotes = await _emoteService.GetEmotes();
-            // получаем текущую локацию пользователя
-            var userLocation = await _locationService.GetUserLocation((long) context.User.Id);
+            // получаем пользователя
+            var user = await _userService.GetUser((long) context.User.Id);
             // получаем доступные в этой локации контракты
-            var contracts = await _contractService.GetContract(userLocation);
+            var contracts = await _contractService.GetContract(user.Location);
 
             var embed = new EmbedBuilder()
                 // баннер контрактов
@@ -68,13 +75,20 @@ namespace Hinode.Izumi.Services.Commands.UserCommands.ContractCommands.ContractL
             {
                 // получаем репутацию в этой локации
                 var reputationType = _reputationService.GetReputationByLocation(contract.Location);
+                // определяем длительность контракта
+                var contractTime = _calc.ActionTime(contract.Time, user.Energy);
+                // заполняем длительность контракта
+                var contractTimeString = contractTime == contract.Time
+                    ? contract.Time.Hours().Humanize(1, new CultureInfo("ru-RU"))
+                    : $"~~{contract.Time.Hours().Humanize(1, new CultureInfo("ru-RU"))}~~ {contractTime.Hours().Humanize(1, new CultureInfo("ru-RU"))}";
+
                 // заполняем информацию о контракте
                 embed.AddField($"{emotes.GetEmoteOrBlank("List")} `{contract.Id}` {contract.Name}",
                     IzumiReplyMessage.ContractListFieldDesc.Parse(
                         contract.Description, emotes.GetEmoteOrBlank(Currency.Ien.ToString()), contract.Currency,
                         _local.Localize(Currency.Ien.ToString(), contract.Currency),
                         emotes.GetEmoteOrBlank(reputationType.Emote(long.MaxValue)), contract.Reputation,
-                        contract.Location.Localize(true), contract.Time.Hours().Humanize(1, new CultureInfo("ru-RU"))));
+                        contract.Location.Localize(true), contractTimeString));
             }
 
             await _discordEmbedService.SendEmbed(context.User, embed);
