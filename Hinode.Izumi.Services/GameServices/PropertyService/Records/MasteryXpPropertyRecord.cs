600471f2807c722874100bc5d0a78c3faedf54b3
Холли <kkxo.mail@gmail.com>
2021-06-03 16:48:21 +0300

    HD-267. Исправление начисления мастерства пользователю.

--- Hinode.Izumi.Services/GameServices/PropertyService/Records/MasteryXpPropertyRecord.cs
+++ Hinode.Izumi.Services/GameServices/PropertyService/Records/MasteryXpPropertyRecord.cs
@@ -1,12 +1,12 @@
 ﻿namespace Hinode.Izumi.Services.GameServices.PropertyService.Records
 {
     public record MasteryXpPropertyRecord(
-        long Mastery0,
-        long Mastery50,
-        long Mastery100,
-        long Mastery150,
-        long Mastery200,
-        long Mastery250)
+        double Mastery0,
+        double Mastery50,
+        double Mastery100,
+        double Mastery150,
+        double Mastery200,
+        double Mastery250)
     {
         public MasteryXpPropertyRecord() : this(default, default, default, default, default, default)
         {
